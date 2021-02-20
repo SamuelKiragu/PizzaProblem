664ddc37b31f140ab38a8f0113f453d518dbc3d2
Kiragu <samuelkiragu107@gmail.com>
2021-02-20 21:39:44 +0300

    first commit

---
+++ csv_reader.py
@@ -0,0 +1,38 @@
+import csv
+import operator
+
+class Pizza:
+    # attributes for pizza
+    # number available
+    # toppings
+    def __init__(self, numb, toppings):
+        self.numb = numb
+        self.toppings = toppings
+        self.topping_no = len(self.toppings)
+        print(self.topping_no)
+
+    def __repr__(self):
+        return f'{self.numb},{self.toppings}'
+
+# array containing important info
+# [0] contains pizza_number
+# [1:3] contains team values for teams
+info =[0,0,0,0]
+
+# array containing info
+# about available pizzas
+pizza_arr = []
+
+with open('a_example') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter = " ")
+    count = 0
+    for row in csv_reader:
+        if count == 0:
+            index = count
+            for col in row:
+                info[index] = col
+                index += 1
+        else:
+            # create a pizza object and add it to the list of pizzas
+            pizza_arr.append(Pizza(row[0], [i for i in row if not(row.index(i) == 0)]))
+        count += 1
